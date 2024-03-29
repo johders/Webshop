4ecca322ff46ad4d95fc24f74b705e7fe1748a34
jd-HW <johannes.dereuddre@student.howest.be>
2024-03-29 11:52:04 +0100

    Update category class repo check

--- PE1.Webshop.Core/Category.cs
+++ PE1.Webshop.Core/Category.cs
@@ -10,7 +10,8 @@ namespace PE1.Webshop.Core
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Image { get { return $"~/images/roast-{Id}.jpg"; } }
-        public IEnumerable<Coffee> Coffees { get; set; }
-    }
+		public string ImageString { get; set; }
+
+		public ICollection<Coffee> Coffees { get; set; }
+	}
 }
