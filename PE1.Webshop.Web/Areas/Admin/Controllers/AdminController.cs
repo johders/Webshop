1442057fd0c96e59ccc5f3fce1721e9233ede75d
jd-HW <johannes.dereuddre@student.howest.be>
2024-04-04 11:11:25 +0200

    Add admin page index list view

---
+++ PE1.Webshop.Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PE1.Webshop.Web.Data;
+using PE1.Webshop.Web.ViewModels;
+
+namespace PE1.Webshop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+
+        private readonly CoffeeShopContext _coffeeShopContext;
+
+        public AdminController(CoffeeShopContext coffeeShopContext)
+        {
+            _coffeeShopContext = coffeeShopContext;
+        }
+
+        public IActionResult Index()
+        {
+
+            var coffees = _coffeeShopContext.Coffees
+            .Select(coffee => new ProductsCoffeeDetailsViewModel
+            {
+                Id = coffee.Id,
+                Name = coffee.Name,
+                Description = coffee.Description,
+                Origin = coffee.Origin,
+                Price = coffee.Price,
+                Category = coffee.Category,
+                Properties = coffee.Properties,
+                ImageString = coffee.ImageString,
+                CertifiedOrganic = coffee.CertifiedOrganic
+            });
+
+            var allCoffeesModel = new ProductsAllCoffeesViewModel
+            {
+                AllCoffees = coffees.ToList()
+            };
+
+            return View(allCoffeesModel);
+
+
+            return View();
+        }
+    }
+}
