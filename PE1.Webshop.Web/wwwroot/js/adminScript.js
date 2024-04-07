5bcd727a89a0d0fa99dcd41c2a365292c1a8cfe8
jd-HW <johannes.dereuddre@student.howest.be>
2024-04-07 10:27:10 +0200

    Implement js toggle script for multiselect

---
+++ PE1.Webshop.Web/wwwroot/js/adminScript.js
@@ -0,0 +1,31 @@
+﻿let allOptions;
+
+window.addEventListener("load", initialize);
+
+function initialize() {
+    console.log("Loaded");
+
+    allOptions = document.querySelectorAll(".prop-item");
+
+    console.log(allOptions);
+
+    addCickEvents();
+}
+
+
+function addCickEvents() {
+
+    allOptions.forEach(option => { option.addEventListener("click", selectOrDeselect)});
+
+}
+
+function selectOrDeselect() {
+    console.log("yay");
+
+    if (this.classList.contains("prop-item-selected")) {
+        this.classList.remove("prop-item-selected")
+    }
+    else {
+        this.classList.add("prop-item-selected")
+    }
+}
